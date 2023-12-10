01b0601ae60c4fbf77547fbf9914641dbbf22048
Aleksandar Konstantinov Yanev <aleyan@gmail.com>
2023-12-10 22:00:53 +0200

    Upload a first solution.

---
+++ Programming Advanced for QA/DefiningClasesLab/CarManufacturer/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        string make;
+
+       public string Make
+        { 
+            get { return make; } 
+            set {  make = value; } 
+        } 
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+    }
+}
