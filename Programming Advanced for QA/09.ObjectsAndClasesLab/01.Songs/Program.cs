888767545c646faf67ea88088419006f789fa593
Aleksandar Konstantinov Yanev <aleyan@gmail.com>
2024-01-15 09:50:53 +0200

    Upload a solusions

---
+++ Programming Advanced for QA/09.ObjectsAndClasesLab/01.Songs/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _01.Songs
+{
+    internal class Song
+    {
+        public string TypeList { get; set; }
+        public string Name { get; set; }
+        public string Time { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOFSongs = int.Parse(Console.ReadLine());
+            List<Song> listOfSongs = new List<Song>();
+
+            for (int i = 0; i < numberOFSongs; i++)
+            {
+                string[] partsOfSong = Console.ReadLine().Split("_").ToArray();
+                Song currentSong = new Song() 
+                { 
+                    TypeList = partsOfSong[0],
+                    Name = partsOfSong[1],
+                    Time = partsOfSong[2]
+                };
+                
+                listOfSongs.Add(currentSong);
+            }
+
+            string command = Console.ReadLine();
+
+            if (command != "all")
+            {
+                listOfSongs = listOfSongs.Where(x => x.TypeList == command).ToList();
+            }
+
+            foreach (Song song in listOfSongs)
+            {
+                Console.WriteLine(song.Name);
+            }
+        }
+    }
+}
