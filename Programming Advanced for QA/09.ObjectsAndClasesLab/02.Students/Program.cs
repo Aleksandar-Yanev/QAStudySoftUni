bff40d6319bdd34a17c3daad931a17ced62c878b
Aleksandar-Yanev <136829077+Aleksandar-Yanev@users.noreply.github.com>
2024-01-16 10:26:27 +0200

    Remove file

--- Programming Advanced for QA/09.ObjectsAndClasesLab/02.Students/Program.cs
+++ Programming Advanced for QA/09.ObjectsAndClasesLab/02.Students/Program.cs
@@ -1,10 +1,42 @@
 ﻿namespace _02.Students
 {
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }    
+        public string HomeTown { get; set; }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input = Console.ReadLine();
+            List<Student> listOfStudents = new List<Student>();
+
+            while (input != "end")
+            {
+                string[] studentData = input.Split(" ").ToArray();
+                Student currentStudent = new Student() 
+                {
+                    FirstName = studentData[0],
+                    LastName = studentData[1],
+                    Age = int.Parse(studentData[2]),
+                    HomeTown = studentData[3]
+                };
+
+                listOfStudents.Add(currentStudent);
+
+                input = Console.ReadLine();
+            }
+
+            string sity = Console.ReadLine();
+            listOfStudents = listOfStudents.Where(x => x.HomeTown == sity).ToList();
+            foreach (Student student in listOfStudents)
+            {
+                Console.WriteLine(student.FirstName + " " + student.LastName + " is " + student.Age + " years old.");
+            }
+           
         }
     }
 }
---
+++ Programming Advanced for QA/09.ObjectsAndClasesLab/03.StoreBoxes/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _03.StoreBoxes
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+
+    public class Box
+    {
+        public string SerialNumber { get; set; }
+        public string Item { get; set; }
+        public int ItemQuantity { get; set; }
+        public double PriceForABox { get; set; }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            
+
+            while (input == "end")
+            {
+
+            }
+        }
+    }
+}
