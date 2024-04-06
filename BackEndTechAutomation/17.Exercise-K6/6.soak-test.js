3442b00b6ed1ef578b5c8cfd0928b4ff5f3a550b
Aleksandar-Yanev <136829077+Aleksandar-Yanev@users.noreply.github.com>
2024-04-06 10:46:51 +0300

    Add soak test script

---
+++ BackEndTechAutomation/17.Exercise-K6/6.soak-test.js
@@ -0,0 +1,30 @@
+import http from 'k6/http'
+import { sleep } from 'k6'
+
+export const options = {
+    stages:[
+        {
+            target: 1000,
+            duration: '5m'
+        },
+        {
+            target: 1000,
+            duration: '24h'
+        },
+        {
+            target: 0,
+            duration: '5m'
+        }
+    ]
+}
+
+export default function () {
+    http.get('https://test.k6.io')
+    sleep(1)
+    
+    http.get('https://test.k6.io/contact')
+    sleep(2)
+
+    http.get('https://test.k6.io/news')
+    sleep(2)
+}
